646f00f99246ffad15f5bdca326e4cc23a4d7cc4
David Zoufalý <davidzoufaly@gmail.com>
2019-04-15 16:14:44 +0200

    Styling and animation - icon added, toggle icon animation added, fade in content animation added, elements styling

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -5,19 +5,48 @@ const contentData = {
   DOMElement: document.querySelector("#content")
 };
 
+//! Universal method for fade in effect
+let opacity;
+const fadeFunction = el => {
+  if (opacity < 0.9) {
+    opacity += 0.1;
+    setTimeout(() => fadeFunction(el), 50);
+  }
+  el.style.opacity = opacity;
+};
+
 //! Universal method for content toggle - show / hide
 const toggleContent = (text, elementInDOM) => {
   // Return statement of this function with ternary operator.
   return (
-    // If element does not have children add content into DOM
+    // If element does not have children (is empty) add content into DOM...
     elementInDOM.children.length === 0
-      ? (elementInDOM.innerHTML = `<div>
+      ? ((elementInDOM.innerHTML = `<div>
                     <p>${text}</p>
-                </div>`)
+                </div>`),
+        //...and show with fade effect
+        (opacity = 0),
+        fadeFunction(elementInDOM))
       : // Otherwise remove it from DOM
         elementInDOM.removeChild(elementInDOM.childNodes[0])
   );
 };
+
+//! Handling CSS keyframe based animation with classes
+function handlingAnimation(icon) {
+  if (icon.contains("container__title__icon--animation--forward")) {
+    icon.toggle("container__title__icon--animation--back");
+    setTimeout(() => {
+      //wait for animation is done
+      icon.toggle("container__title__icon--animation--forward");
+    }, 300);
+  } else if (icon.contains("container__title__icon--animation--back")) {
+    icon.toggle("container__title__icon--animation--forward");
+    icon.toggle("container__title__icon--animation--back");
+  } else {
+    icon.toggle("container__title__icon--animation--forward");
+  }
+}
 
 //! Destructuring data object
 const { text, DOMElement } = contentData;
@@ -25,9 +54,10 @@ const { text, DOMElement } = contentData;
 //! Handling clicks on title
 // 1) Select our title element in DOM
 document
-.querySelector("#toggle-content-title")
-// 2) Add event listener with click trigger
-.addEventListener("click", function() {
-    // 3) callback fucntion running toggleContent method with our data passed as parameters
+  .querySelector("#toggle-content-title")
+  // 2) Add event listener with click trigger
+  .addEventListener("click", function() {
+    // 3) callback fucntion running toggleContent method with our data passed as parameters and hadnlingAmination method with our span passed as parameter
     toggleContent(text, DOMElement), false;
+    handlingAnimation(this.childNodes[1].classList);
   });
